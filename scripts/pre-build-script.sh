4fff238cf5e8af25c5f4bf8490249e1559f37984
phamLinh98 <linhthusinh98@gmail.com>
2025-05-29 17:05:14 +0900

    Refactor pre-build-script.sh for improved clarity and consistency

--- scripts/pre-build-script.sh
+++ scripts/pre-build-script.sh
@@ -1,13 +1,34 @@
+# #!/bin/bash
+
+# echo "Pre-build script is running..."
+
+# # Prebuild Pharse
+# echo "Running pre-build pharse"
+# npx tsc
+
+# echo "CDK bootstrap"
+# cdk bootstrap aws://650251698778/ap-northeast-1
+
+# echo "Bundle source code"
+# npm run bundle-esbuild
+
+
 #!/bin/bash
 
 echo "Pre-build script is running..."
 
-# Prebuild Pharse
-echo "Running pre-build pharse"
-npx tsc
+# Prebuild Phase
+echo "Running pre-build phase"
+
+# Vào repo lambda-project trước
+cd /codebuild/lambda-source
+
+# Bundle source code tại repo lambda-project
+npm install
+npm run bundle-esbuild
+
+# Quay lại repo chính (cdk-project) để deploy
+cd /codebuild/input
 
 echo "CDK bootstrap"
 cdk bootstrap aws://650251698778/ap-northeast-1
-
-echo "Bundle source code"
-npm run bundle-esbuild
